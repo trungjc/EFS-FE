e3dde52efa155b3e34a7cb63d05c1f93f449a494
hieudt7 <hieudtx9@gmail.com>
2018-10-24 23:07:49 +0700

    add range js

---
+++ src/js/new-function.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    var slider = document.getElementById('priceRange');
+    noUiSlider.create(slider, {
+        start: [0, 100],
+        connect: true,
+        step: 1,
+        range: {
+            'min': 0,
+            'max': 100
+        },
+        format: wNumb({
+            decimals: 0
+        })
+    });
+    slider.noUiSlider.on('update', function () {
+    });
+})
